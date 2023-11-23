c2ff8c7384b630e1a3cff8e91185e5957a1c824b
adityatiwari66 <adityatiwari06a@gmail.com>
2023-11-23 17:49:12 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+  
+    printf("enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    c=a+b;  printf("the value of c is %d",c);
+    return 0;
+}
