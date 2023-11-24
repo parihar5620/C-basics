7b932b7643a4e8c73fa2dd8e3ce6b950f1722852
Tribhuvan Singh Parihar <122529302+parihar5620@users.noreply.github.com>
2023-11-24 12:23:40 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int number1, number2, multiplication;
+
+    printf("Enter two integers:");
+    scanf("%d %d",&number1, &number2);
+
+    //calculate sub;
+ multiplication = number1 *number2;
+
+    printf("%d",multiplication);
+
+    return 0;
+}
